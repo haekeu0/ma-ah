10715bc12d4ae183d0346238081658355ddeb469
agnyang <hyang2@kumefood.comm>
2023-11-21 10:50:37 +0900

    천재 2

--- maah/lib/main.dart
+++ maah/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             // 나는 천재야
+            // 나도
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
